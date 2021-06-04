1d729d2a16161ceaefbad3a17299d282fcae85d0
Tobse <Tobse4Git@gmail.com>
2021-06-04 18:22:28 +0200

    📏 Resize game window

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -8,7 +8,10 @@ import com.soywiz.korio.file.std.*
 import com.soywiz.korma.geom.degrees
 import com.soywiz.korma.interpolation.Easing
 
-suspend fun main() = Korge(width = 512, height = 512, bgcolor = Colors["#2b2b2b"]) {
+val gameWidth = 540
+val gameHeight = 920
+
+suspend fun main() = Korge(width = gameWidth, height = gameHeight, bgcolor = Colors["#2b2b2b"]) {
 	val minDegrees = (-16).degrees
 	val maxDegrees = (+16).degrees
 
